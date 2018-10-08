2a9e9a389a79ac2aef58788a24bd14881f33708e
Thomas Hanna <thomasjhanna@gmail.com>
2018-10-08 12:03:18 +0100

    Added media queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
+
+const StyledLink = styled(Link)`
+  color: red;
+  text-decoration: none;
+`;
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <StyledLink to="/page-2/">Go to page 2</StyledLink>
     <br />
     <Link to="/page-3/">Go to page 3</Link>
     <br />
