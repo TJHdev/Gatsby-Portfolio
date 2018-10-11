b3a2d92f9f2e011c678b31c84201672a1b82c143
Thomas Hanna <thomasjhanna@gmail.com>
2018-10-11 21:35:31 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-825b6448eb1847fd388f.js"
+    "url": "webpack-runtime-ceca079213a1f9fe9290.js"
   },
   {
     "url": "app-7ddcfd07fad99056a294.js"
@@ -36,17 +36,17 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "0976788499def3099f5c13e33fa5c48e"
+    "revision": "722c58282470a6861d62e46f9d464c5e"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "017c8ddcaeedeb3c9a38e84e9258d83d"
+    "revision": "1205c5c3ff355baf18fe726f3ca507f4"
   },
   {
     "url": "component---src-pages-index-js.f789a2ce12a4190ab101.css"
   },
   {
-    "url": "component---src-pages-index-js-a4b550c9be5368aefa44.js"
+    "url": "component---src-pages-index-js-337b48b0eae1106f1740.js"
   },
   {
     "url": "4-77286eb3d6dda4779f59.js"
